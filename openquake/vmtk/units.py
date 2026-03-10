61b018899c022684c14bdd485b38b2703870ed7d
mouayed-nafeh <149155077+mouayed-nafeh@users.noreply.github.com>
2026-03-10 15:28:57 +0100

    Refactor units code structure for improved readability and maintainability according to PEP8 standards

--- openquake/vmtk/units.py
+++ openquake/vmtk/units.py
@@ -1,44 +1,45 @@
 import numpy as np
 
+
 class units():
-    
+
     """
     Standardised units in terms of m and kn for Opensees applications
     """
     pi = np.pi
     g = 9.81
-    
+
     # Length
     m = 1.0
     mm = m/1000.
     cm = m/100.
     inch = 25.4*mm
     ft = 12*inch
-    
+
     # Area
     m2 = np.power(m, 2)
     mm2 = np.power(mm, 2)
     cm2 = np.power(cm, 2)
     inch2 = np.power(inch, 2)
-    
+
     # Second Moment of Area
     m4 = np.power(m, 4)
     cm4 = np.power(cm, 4)
     mm4 = np.power(mm, 4)
     inch4 = np.power(inch, 4)
-    
+
     # Force
     kN = 1.0
     N = kN/1000.
     kips = kN*4.448221615
-    
+
     # Moment
     kNm = kN*m
-    
+
     # Mass (tonnes)
-    tonne = 1.0;
+    tonne = 1.0
     kg = tonne/1000.
-    
+
     # Stress (kN/m2 or kPa)
     kNm2 = kN/m2
     Pa = N/m2
@@ -49,7 +50,7 @@ class units():
     GPa = Pa*1.0e9
     ksi = 6.8947573*MPa
     kgcm2 = kg*g/cm2
-    
+
     # Angles
     degrees = pi/180.
 
